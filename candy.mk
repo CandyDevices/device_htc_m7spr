e6ada247af7c9caf2a19672569b2272f50677b03
gimme <gimmeitorilltell@gmail.com>
2018-04-11 02:18:06 -0400

    Add telephony boot jars & pkgs
    Change-Id: I2bf2ae8cad64c49f0ce5eaa6f5572e1cbf4cd52b

--- candy.mk
+++ candy.mk
@@ -1,5 +1,7 @@
 $(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
+$(call inherit-product, vendor/candy/config/caf_fw.mk)
+
 $(call inherit-product, vendor/candy/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
